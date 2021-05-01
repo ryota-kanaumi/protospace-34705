9a549592c164e8cb9a3427d0991c96590fd50f80
ryota-kanaumi <ayusan.0729@icloud.com>
2021-05-01 12:12:06 +0900

    ログインしていなくてもコメント表示される

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -58,7 +58,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action :show
+      redirect_to action :index
     end
   end
 end
