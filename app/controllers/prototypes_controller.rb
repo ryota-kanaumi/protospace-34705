0262839581c968ab487d3911122388486afbce78
ryota-kanaumi <ayusan.0729@icloud.com>
2021-05-01 11:03:28 +0900

    ユーザの詳細ページヘの遷移の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -55,7 +55,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to action :show
     end
   end
 end
